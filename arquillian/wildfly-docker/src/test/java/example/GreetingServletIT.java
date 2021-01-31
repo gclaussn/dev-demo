b80190d6c9d13c7d84686746f36f239e39ec3ec3
Gabor Claussnitzer <gabor.claussnitzer@googlemail.com>
2021-01-31 17:06:40 +0100

    [Arquillian] Upgraded to JUnit 4.13.1

--- arquillian/wildfly-docker/src/test/java/example/GreetingServletIT.java
+++ arquillian/wildfly-docker/src/test/java/example/GreetingServletIT.java
@@ -1,7 +1,7 @@
 package example;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
--- arquillian/wildfly-remote/src/test/java/example/TodoResourceStubIT.java
+++ arquillian/wildfly-remote/src/test/java/example/TodoResourceStubIT.java
@@ -3,7 +3,7 @@ package example;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
 import java.net.URL;
